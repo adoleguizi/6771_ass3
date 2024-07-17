f89021b706bb52e05a3fd0fa7d2c9425cc643a2f
Zhihao Sun <z5456123@ad.unsw.edu.au>
2024-07-18 00:33:52 +1000

    Make graph and edge  template <N,W> using list initialization to value initialise

--- src/gdwg_graph.h
+++ src/gdwg_graph.h
@@ -1,23 +1,39 @@
 #ifndef GDWG_GRAPH_H
 #define GDWG_GRAPH_H
-
+#include <memory>
+#include <vector>
 // TODO: Make both graph and edge generic
 //       ... this won't just compile
 //       straight away
 namespace gdwg {
+	// 前向声明graph模板，以便在edge中使用
+	template<typename N, typename W>
+	class graph;
+
+	template<typename N, typename W>
 	class edge {
 	 public:
 	 private:
 		// You may need to add data members and member functions
-		friend class graph;
+		friend class graph<N, W>;
 	};
+
+	template<typename N, typename W>
 	class graph {
 	 public:
-		using edge = gdwg::edge;
-
+		using edge = gdwg::edge<N, W>;
+		graph();
 		// Your member functions go here
 	 private:
+		std::vector<N> nodes_;
+		std::vector<std::unique_ptr<edge>> edges_;
 	};
 } // namespace gdwg
 
+template<typename N, typename W>
+gdwg::graph<N, W>::graph()
+: nodes_()
+, edges_() {
+	// Constructor
+}
 #endif // GDWG_GRAPH_H
