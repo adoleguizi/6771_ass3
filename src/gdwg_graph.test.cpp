a81e4db550c180124984df0161302897d47c81f8
Zhihao Sun <z5456123@ad.unsw.edu.au>
2024-07-21 18:20:31 +1000

    add two testcases insert_node is is_node insert duplicate edge weight in int not string

--- src/gdwg_graph.test.cpp
+++ src/gdwg_graph.test.cpp
@@ -16,7 +16,7 @@ TEST_CASE("graph range constructor") {
 	}
 }
 // test copy constructor after defined inert_edge to copy
-TEST_CASE("graph copy constructor") {
+TEST_CASE("graph copy constructor and assign operator") {
 	auto g1 = gdwg::graph<int, std::string>{1, 2, 3};
 	g1.insert_edge(1, 2);
 	g1.insert_edge(2, 3, "weight1");
@@ -28,4 +28,27 @@ TEST_CASE("graph copy constructor") {
 	CHECK(g2.is_node(3));
 	CHECK(g2.insert_edge(1, 2) == false);
 	CHECK(g2.insert_edge(2, 3, "weight1") == false);
+}
+TEST_CASE("insert node and check node") {
+	auto g = gdwg::graph<int, std::string>{};
+	auto n1 = 1;
+	auto n2 = 2;
+	auto n3 = 3;
+
+	g.insert_node(n1);
+	g.insert_node(n2);
+	g.insert_node(n3);
+
+	CHECK(g.is_node(n1));
+	CHECK(g.is_node(n2));
+	CHECK(g.is_node(n3));
+	CHECK(!g.is_node(4));
+}
+TEST_CASE("insert duplicate edges weight int and unweighted edge") {
+	auto g = gdwg::graph<int, int>{1, 2, 3};
+	g.insert_edge(1, 2, 10);
+	CHECK(g.insert_edge(1, 2, 10) == false);
+
+	g.insert_edge(2, 3);
+	CHECK(g.insert_edge(2, 3) == false);
 }
