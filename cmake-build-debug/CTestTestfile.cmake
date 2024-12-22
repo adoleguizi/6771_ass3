7abeb8b2e5d25cb865119a93e72b341c390ec40c
adoleguizi <t3e7899@163.com>
2024-12-22 16:26:05 +0800

    Initial commit

---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /mnt/e/6771_T2/ass3
+# Build directory: /mnt/e/6771_T2/ass3/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(gdwg_graph_test "gdwg_graph_test_exe")
+set_tests_properties(gdwg_graph_test PROPERTIES  _BACKTRACE_TRIPLES "/mnt/e/6771_T2/ass3/CMakeLists.txt;71;add_test;/mnt/e/6771_T2/ass3/CMakeLists.txt;0;")
